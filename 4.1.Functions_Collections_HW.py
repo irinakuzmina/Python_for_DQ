8bed25da97d57bea17dc8720d29cf11272e4e05a
Iryna_Kuzmina <iryna.kuzmina@spglobal.com>
2023-02-22 19:27:01 +0300

    HW for module #4

---
+++ 4.1.Functions_Collections_HW.py
@@ -0,0 +1,58 @@
+# Task 1. Create a list of random number of dicts (from 2 to 10)
+# dict's random numbers of keys should be letter, dict's values should be a number (0-100),
+# example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+
+import random  # to import random module which is used to generate random numbers
+import string  # to import string module which to use string.ascii_lowercase constants to return lowercase letters
+
+
+def dict_list_creation():
+    rand_list = []
+    for i in range(random.randint(2, 10)):  # loop to add dictionaries to the list with append() method
+        rand_dict = {}
+        for j in range(0, 3):  # loop to add pairs key-value to each dictionary
+            rand_dict.update({random.choice(string.ascii_lowercase): random.randint(0, 100)})  # to add pairs key-value
+        rand_list.append(rand_dict)  # to add dictionaries to the list with append() method
+    return rand_list  # to print to console created list of dictionaries
+
+
+print(dict_list_creation())
+
+# Task 2. get previously generated list of dicts and create one common dict:
+# if dicts have same key, we will take max value, and rename key with dict number with max value
+# if key is only in one dict - take it as is, example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
+
+
+def common_dict_creation():
+    common_dict = {}  # to create empty dictionary
+    random_list = dict_list_creation()
+    h_list = random_list
+
+    for i in random_list:  # loop to check each dictionary from rand_list
+        h_list = [
+            item for item in h_list if item != i
+        ]  # list comprehension iteratively removing items from h_list
+        # print(h_list)  # to check h_list in console
+        if len(h_list) != 0:  # statement to check if h_list is not empty
+            for k in h_list:  # loop to check each dictionary in h_list
+                for key, value in i.items():  # to compare keys from current dictionary with others
+                    for key1, value1 in k.items():
+                        if key == key1:  # if statement
+                            if value >= value1:
+                                # k.pop(key1)
+                                key = key+'_'+str(random_list.index(i)+1)  # to rename key if 1st value is bigger
+                                common_dict.update({key: value})  # to update common_dict with key+value pair
+                            else:
+                                # i.pop(key)
+                                key1 = key1+'_'+str(random_list.index(k)+1) # to rename key if 2nd value is bigger
+                                common_dict.update({key1: value1})
+                        else:
+                            common_dict.update({key: value})  # to update common_dict with key+value pair if key is unique
+        else:
+            for key, value in i.items():
+                common_dict.update({key: value})  # to add left after comparison pairs to common_dict
+
+    print(common_dict)
+
+
+common_dict_creation()
